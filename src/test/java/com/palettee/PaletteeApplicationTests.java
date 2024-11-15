31d30a74ada148bfcf705e1a1f421b286fc58c6f
jbw9964 <jbw9964@gmail.com>
2024-11-15 14:30:29 +0900

    gradle `ignoreFailures` 설정 추가
    + 샘플 test 들 추가

--- src/test/java/com/palettee/PaletteeApplicationTests.java
+++ src/test/java/com/palettee/PaletteeApplicationTests.java
@@ -10,4 +10,28 @@ class PaletteeApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void sampleTest1()	{
+	}
+
+	@Test
+	void sampleTest2()	{
+	}
+
+	@Test
+	void sampleTest3()	{
+	}
+
+	@Test
+	void sampleTest4()	{
+	}
+
+	@Test
+	void sampleTest5()	{
+	}
+
+	@Test
+	void sampleTest6()	{
+		assert true == false;
+	}
 }
