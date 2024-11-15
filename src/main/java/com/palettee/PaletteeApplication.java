202bc667c4e74818b3f76f347fe0b2ffb8f329fd
Taaate <an981022@naver.com>
2024-11-15 14:09:13 +0900

    Initial Commit

---
+++ src/main/java/com/palettee/PaletteeApplication.java
@@ -0,0 +1,13 @@
+package com.palettee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaletteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaletteeApplication.class, args);
+	}
+
+}
