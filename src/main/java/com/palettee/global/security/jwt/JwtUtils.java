56a2419a3aa3fffd4b9d51735e9a929f60d7cd11
청주는사과아님 <jbw9964@gmail.com>
2024-11-20 10:39:42 +0900

    Feat/#14 - `OAuth2` 로그인 기능과 `Jwt` 편의 기능 추가 (#20)
    * feat: 기본 `SecurityConfig` + `OAuth2` 인증 기능 추가
     - `OAuth2` 성공, 실패 handler 추가
    
    * feat: `Jwt` 검증, 발급용 클래스 추가
    
    * feat: `User` `Entity` 에 필요 속성 추가 및 refactor
    - UserRole : 유저 권한 구분용 `enum`
    - User#oauthIdentity : OAuth provider + providerId 로 구성된 또다른 식별자 column
    
    * ci: `ci` 시 필요한 `Secret` 들 가져오도록 설정

---
+++ src/main/java/com/palettee/global/security/jwt/JwtUtils.java
@@ -0,0 +1,101 @@
+package com.palettee.global.security.jwt;
+
+import com.palettee.user.domain.*;
+import lombok.extern.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+/**
+ * Jwt 토큰과 관련된 {@code Component}
+ *
+ * @see CustomJwtUtil
+ */
+@Slf4j
+@Component
+public class JwtUtils {
+
+    private final CustomJwtUtil temporaryJwtUtil;
+    private final CustomJwtUtil accessJwtUtil;
+    private final CustomJwtUtil refreshJwtUtil;
+
+    private final long tempoExpireMin;
+    private final long accessExpireMin;
+    private final long refreshExpireMin;
+
+    public JwtUtils(
+            @Value("${jwt.temporary.secret}") String tempoSecret,
+            @Value("${jwt.temporary.expire-time-min}") long tempoExpireMin,
+            @Value("${jwt.access.secret}") String accessSecret,
+            @Value("${jwt.access.expire-time-min}") long accessExpireMin,
+            @Value("${jwt.refresh.secret}") String refreshSecret,
+            @Value("${jwt.refresh.expire-time-min}") long refreshExpireMin
+    ) {
+        this.temporaryJwtUtil = new CustomJwtUtil(tempoSecret);
+        this.accessJwtUtil = new CustomJwtUtil(accessSecret);
+        this.refreshJwtUtil = new CustomJwtUtil(refreshSecret);
+
+        this.tempoExpireMin = tempoExpireMin;
+        this.accessExpireMin = accessExpireMin;
+        this.refreshExpireMin = refreshExpireMin;
+    }
+
+    // 임시 토큰과 관련된 utils
+    public boolean isTemporaryTokenValid(String temporaryToken) {
+        return temporaryJwtUtil.isValid(temporaryToken);
+    }
+
+    public boolean isTemporaryTokenExpired(String temporaryToken) {
+        return temporaryJwtUtil.isExpired(temporaryToken);
+    }
+
+    public String createTemporaryToken(User user) {
+        return temporaryJwtUtil.createToken(user, tempoExpireMin);
+    }
+
+    /**
+     * @see CustomJwtUtil#getEmail(String)
+     */
+    public String getEmailFromTemporaryToken(String temporaryToken) {
+        return temporaryJwtUtil.getEmail(temporaryToken);
+    }
+
+    // access 토큰과 관련된 utils
+    public boolean isAccessTokenValid(String accessToken) {
+        return accessJwtUtil.isValid(accessToken);
+    }
+
+    public boolean isAccessTokenExpired(String accessToken) {
+        return accessJwtUtil.isExpired(accessToken);
+    }
+
+    public String createAccessToken(User user) {
+        return accessJwtUtil.createToken(user, accessExpireMin);
+    }
+
+    /**
+     * @see CustomJwtUtil#getEmail(String)
+     */
+    public String getEmailFromAccessToken(String accessToken) {
+        return accessJwtUtil.getEmail(accessToken);
+    }
+
+    // refresh 토큰과 관련된 utils
+    public boolean isRefreshTokenValid(String refreshToken) {
+        return refreshJwtUtil.isValid(refreshToken);
+    }
+
+    public boolean isRefreshTokenExpired(String refreshToken) {
+        return refreshJwtUtil.isExpired(refreshToken);
+    }
+
+    public String createRefreshToken(User user) {
+        return refreshJwtUtil.createToken(user, refreshExpireMin);
+    }
+
+    /**
+     * @see CustomJwtUtil#getEmail(String)
+     */
+    public String getEmailFromRefreshToken(String refreshToken) {
+        return refreshJwtUtil.getEmail(refreshToken);
+    }
+}
