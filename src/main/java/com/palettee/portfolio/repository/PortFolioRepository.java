e92b618984b315da47bcb1a85fc2c20e2f5b13e7
jbw9964 <jbw9964@gmail.com>
2024-11-25 16:19:10 +0900

    feat: `Repository` 필요한 메서드 추가

--- src/main/java/com/palettee/portfolio/repository/PortFolioRepository.java
+++ src/main/java/com/palettee/portfolio/repository/PortFolioRepository.java
@@ -1,7 +1,11 @@
 package com.palettee.portfolio.repository;
 
-import com.palettee.portfolio.domain.PortFolio;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.palettee.portfolio.domain.*;
+import org.springframework.data.jpa.repository.*;
 
 public interface PortFolioRepository extends JpaRepository<PortFolio, Long>, PortFolioRepositoryCustom {
+
+    @Modifying
+    @Query("delete from PortFolio pf where pf.user.id = :userId")
+    void deleteAllByUserId(Long userId);
 }
--- src/main/java/com/palettee/user/repository/RelatedLinkRepository.java
+++ src/main/java/com/palettee/user/repository/RelatedLinkRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.*;
 public interface RelatedLinkRepository
         extends JpaRepository<RelatedLink, Long> {
 
+    @Modifying
+    @Query("delete from RelatedLink rl where rl.user.id = :userId")
+    void deleteAllByUserId(Long userId);
+
 }
