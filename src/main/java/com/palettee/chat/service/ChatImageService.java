d65982dec7746bce00145ab0875c155830df6168
ihun-il <hunil9978@gmail.com>
2024-11-26 15:02:10 +0900

    fix: chatImage 업로드 삭제 - api 통일을 위해 chatImage 삭제

--- src/main/java/com/palettee/chat/service/ChatImageService.java
+++ src/main/java/com/palettee/chat/service/ChatImageService.java
@@ -1,17 +1,12 @@
 package com.palettee.chat.service;
 
-import com.palettee.chat.controller.dto.response.ChatImgUploadResponse;
-import com.palettee.chat.controller.dto.response.ChatImgUrlResponse;
 import com.palettee.chat.domain.ChatImage;
 import com.palettee.chat.repository.ChatImageRepository;
-import com.palettee.global.s3.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +14,8 @@ import java.util.stream.Collectors;
 public class ChatImageService {
 
     private final ChatImageRepository chatImageRepository;
-    private final ImageService imageService;
 
     public List<ChatImage> saveChatImages(List<ChatImage> chatImages) {
         return chatImageRepository.saveAll(chatImages);
-    }
-
-    public ChatImgUploadResponse chatImgUpload(List<MultipartFile> files) {
-        List<ChatImgUrlResponse> chatImgUrls =
-                files.stream()
-                        .map(file -> new ChatImgUrlResponse(imageService.uploadAndMakeImgUrl(file)))
-                        .collect(Collectors.toList());
-
-        return new ChatImgUploadResponse(chatImgUrls);
     }
 }
