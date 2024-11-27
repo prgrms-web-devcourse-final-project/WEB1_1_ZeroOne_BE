24ba5ccc32addc6956b7c791f6a132fea2e310da
ihun-il <hunil9978@gmail.com>
2024-11-27 23:44:56 +0900

    feat: redis pub/sub 사용 - redis pub/sub channel 등록 - reids listner 등록 - redisTemplate 등록 - redis publisher : channel로 publish

--- src/main/java/com/palettee/global/configs/RedisConfig.java
+++ src/main/java/com/palettee/global/configs/RedisConfig.java
@@ -1,14 +1,24 @@
 package com.palettee.global.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.palettee.chat.controller.dto.response.ChatResponse;
+import com.palettee.global.redis.sub.RedisSubscriber;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.*;
 import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.*;
 
 @Configuration
 public class RedisConfig {
+
+    public static final String CHAT_TOPIC = "ChatTopic";
 
     @Value("${redis.host}")
     private String host;
@@ -19,13 +29,35 @@ public class RedisConfig {
 //    @Value("${redis.password}")
 //    private String password;
 
-    @Bean
+    @Bean // Redis 서버와 연결
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
         redisConfiguration.setHostName(host);
         redisConfiguration.setPort(port);
 //        redisConfiguration.setPassword(password);
         return new LettuceConnectionFactory(redisConfiguration);
+    }
+
+    @Bean // Redis Pub/Sub에서 메시지가 교환되는 채널
+    public ChannelTopic channelTopic(){
+        return new ChannelTopic(CHAT_TOPIC);
+    }
+
+    @Bean // Redis Pub/Sub 메시지를 처리하는 리스너를 설정
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber redisSubscriber){
+        // RedisSubscriber 객체를 연결하고 메시지 처리 메서드로 sendMessage를 지정
+        return new MessageListenerAdapter(redisSubscriber,"sendMessage");
+    }
+
+    @Bean // Redis에서 메시지를 구독하고 감지된 메시지를 리스너로 전달하는 컨테이너
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter,
+            ChannelTopic channelTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory); // Redis와 연결 정보를 설정
+        container.addMessageListener(listenerAdapter,channelTopic); // Redis Pub/Sub 채널에서 메시지를 감지하면 리스너로 전달하도록 리스너를 등록
+        return container;
     }
 
     @Bean
@@ -36,5 +68,23 @@ public class RedisConfig {
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, ChatResponse> chatRedisTemplate(RedisConnectionFactory connectionFactory,
+                                                                 @Qualifier("redisObjectMapper") ObjectMapper objectMapper) {
+        RedisTemplate<String, ChatResponse> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean(name = "redisObjectMapper") // LocalDateTime 직렬화 할 때 오류 발생 -> jsr310 Module 추가
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
---
+++ src/main/java/com/palettee/global/redis/sub/RedisSubscriber.java
@@ -0,0 +1,26 @@
+package com.palettee.global.redis.sub;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.palettee.chat.controller.dto.response.ChatResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class RedisSubscriber {
+
+    private final ObjectMapper objectMapper;
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public void sendMessage(String publishMessage) {
+        try {
+            ChatResponse roomMessage = objectMapper.readValue(publishMessage, ChatResponse.class);
+            messagingTemplate.convertAndSend("/sub/chat/" + roomMessage.getChatRoomId(), roomMessage);
+        } catch (Exception e) {
+            log.error("subscriber error : " + e.getMessage());
+        }
+    }
+}
