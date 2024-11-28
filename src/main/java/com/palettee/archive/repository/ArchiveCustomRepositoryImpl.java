cb4d6f810d848e45abca58cb8f189e4c5e6e1708
jbw9964 <jbw9964@gmail.com>
2024-11-28 18:02:21 +0900

    feat: `NoOffset` 을 이용한 검색 메서드 추가
    + 필요한 여러 검색 메서드 추가

--- src/main/java/com/palettee/archive/repository/ArchiveCustomRepositoryImpl.java
+++ src/main/java/com/palettee/archive/repository/ArchiveCustomRepositoryImpl.java
@@ -1,24 +1,18 @@
 package com.palettee.archive.repository;
 
 import static com.palettee.archive.domain.QArchive.*;
-import static com.palettee.user.domain.QUser.user;
+import static com.palettee.user.domain.QUser.*;
 
-import com.palettee.archive.domain.Archive;
-import com.palettee.user.domain.MajorJobGroup;
-import com.palettee.user.domain.MinorJobGroup;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.PathBuilder;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-import org.springframework.stereotype.Repository;
+import com.palettee.archive.domain.*;
+import com.palettee.user.controller.dto.response.*;
+import com.palettee.user.domain.*;
+import com.querydsl.core.types.*;
+import com.querydsl.core.types.dsl.*;
+import com.querydsl.jpa.impl.*;
+import java.time.*;
+import java.util.*;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.*;
 
 @Repository
 public class ArchiveCustomRepositoryImpl implements ArchiveCustomRepository{
@@ -78,6 +72,40 @@ public class ArchiveCustomRepositoryImpl implements ArchiveCustomRepository{
                 .orderBy(sortType("latest"));
 
         return new SliceImpl<>(jpaQuery.fetch(), pageable, slice.hasNext());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GetUserArchiveResponse findArchivesOnUserWithNoOffset(Long userId, int size,
+            Long archiveOffset) {
+
+        // id 내림차순 (최신순) 정렬
+        List<Archive> searchResult = queryFactory
+                .selectFrom(archive)
+                .where(
+                        archive.user.id.eq(userId), // 해당 유저 archive 중
+                        archiveOffset != null ?     // (archive.id <= offset) 인 archive
+                                archive.id.loe(archiveOffset) : null
+                )
+                .orderBy(archive.id.desc())
+                .limit(size + 1)
+                .fetch();
+
+        boolean hasNext = searchResult.size() > size;
+        Long nextOffset = null;
+
+        if (hasNext) {
+            nextOffset = searchResult.get(size).getId();
+            searchResult = searchResult.stream()
+                    .limit(size)
+                    .toList();
+        }
+
+        return GetUserArchiveResponse.of(
+                searchResult, hasNext, nextOffset
+        );
     }
 
     private BooleanExpression majorJobGroupEquals(String majorJobGroup) {
--- src/main/java/com/palettee/portfolio/repository/PortFolioRepository.java
+++ src/main/java/com/palettee/portfolio/repository/PortFolioRepository.java
@@ -1,10 +1,14 @@
 package com.palettee.portfolio.repository;
 
 import com.palettee.portfolio.domain.*;
+import java.util.*;
 import org.springframework.data.jpa.repository.*;
 
 public interface PortFolioRepository extends JpaRepository<PortFolio, Long>,
         PortFolioRepositoryCustom {
+
+    @Query("select p from PortFolio p where p.user.id = :userId")
+    List<PortFolio> findAllByUserId(Long userId);
 
     @Modifying(clearAutomatically = true)
     @Query("delete from PortFolio pf where pf.user.id = :userId")
