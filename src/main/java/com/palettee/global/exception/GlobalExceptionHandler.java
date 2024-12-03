6581f649e2454d34827036df94da93c08a01cd39
청주는사과아님 <jbw9964@gmail.com>
2024-12-04 01:22:29 +0900

    enhance: `GlobalExceptionHandler` 에서 에러 로그 남기도록 개선

--- src/main/java/com/palettee/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/palettee/global/exception/GlobalExceptionHandler.java
@@ -1,22 +1,15 @@
 package com.palettee.global.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.palettee.global.logging.*;
+import jakarta.servlet.http.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.http.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.*;
+import org.springframework.web.servlet.mvc.method.annotation.*;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,18 +17,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // 예상했던 예외들 커스텀
     @ExceptionHandler(PaletteException.class)
-    public ResponseEntity<ErrorResponse> paletteExceptionHandler(PaletteException e) {
+    public ResponseEntity<ErrorResponse> paletteExceptionHandler(
+            PaletteException e, HttpServletRequest request
+    ) {
         ErrorCode code = e.getErrorCode();
         ErrorResponse errorResponse =
                 new ErrorResponse(
                         code.getStatus(),
                         code.getReason());
+
+        this.logError(request, code.getStatus(), code.getReason(), e);
+
         return ResponseEntity.status(HttpStatus.valueOf(code.getStatus())).body(errorResponse);
     }
 
     // 예상치 못했던 예외들은 500에러
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+    protected ResponseEntity<ErrorResponse> handleException(
+            Exception e, HttpServletRequest request
+    ) {
         log.error("INTERNAL_SERVER_ERROR", e);
         ErrorCode internalServerError = ErrorCode.INTERNAL_SERVER_ERROR;
 
@@ -44,29 +44,59 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                         internalServerError.getStatus(),
                         internalServerError.getReason());
 
+        this.logError(request, internalServerError.getStatus(), internalServerError.getReason(), e);
+
         return ResponseEntity.status(HttpStatus.valueOf(internalServerError.getStatus()))
                 .body(errorResponse);
     }
 
-    // valid로 잡힌 예외들 커스텀
+    // @Valid 로 잡힌 예외들 커스텀
     @SneakyThrows
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
 
-        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
-        Map<String, Object> fieldAndErrorMessages =
-                errors.stream()
-                        .collect(
-                                Collectors.toMap(
-                                        FieldError::getField, FieldError::getDefaultMessage));
+        // TODO : 지금 validation 에러가 응답으로 보이긴 하는데 이상함.
+        // 동일한 invalid body 여도 getDefaultMessage 했을 때 오는 메시지가 랜덤함. 어케 고치지???
+        // invalid 한 필드까지는 고정적인 것 같은데 어떤 메시지가 선택되는지가 매번 다름..
+        BindingResult bindingResult = ex.getBindingResult();
 
-        String errorsToJsonString = new ObjectMapper().writeValueAsString(fieldAndErrorMessages);
-        ErrorResponse errorResponse =
-                new ErrorResponse(status.value(), errorsToJsonString);
+        if (!bindingResult.hasErrors()) {
+            log.error(
+                    "Expected request body validation has error but it wasn't. "
+                            + "Unexpected validation detected.");
+        }
+
+        String message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), message);
+
+        HttpServletRequest httpReq = ((ServletWebRequest) request).getRequest();
+
+        this.logError(httpReq, status.value(), message, ex);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    /**
+     * @see MDCLoggingFilter
+     */
+    private void logError(HttpServletRequest req, int status, String msg, Exception e) {
+
+        if (req == null) {
+            log.error("Exception has been occurred : {}, {}", status, msg, e);
+            return;
+        }
+
+        Object requestUUID = req.getAttribute("custom-request-uuid");
+        String httpMethod = req.getMethod();
+        String requestUrl = req.getRequestURL().toString();
+
+        log.error("Exception has been occurred on REQUEST ID [{}] - [{} \"{}\"] : {}, {}",
+                requestUUID != null ? requestUrl : "UNKNOWN",
+                httpMethod, requestUrl,
+                status, msg, e);
     }
 }
