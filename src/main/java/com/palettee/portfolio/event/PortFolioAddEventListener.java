e294482ae93781a323db302dd092330b539b05fb
kcsc2217 <k12002@nate.com>
2025-01-03 22:24:30 +0900

    fix: 포트폴리오 이벤트 걷어냄

--- src/main/java/com/palettee/portfolio/event/PortFolioAddEventListener.java
+++
@@ -1,4 +0,0 @@
-package com.palettee.portfolio.event;
-
-public record PortFolioAddEventListener(Long portFolioId) {
-}
--- src/main/java/com/palettee/portfolio/event/PortFolioUpdateEventListener.java
+++
@@ -1,4 +0,0 @@
-package com.palettee.portfolio.event;
-
-public record PortFolioUpdateEventListener(Long portFolioId) {
-}
